df09f028723fa44a98582822490f0e0a8181cfac
jiko21 <daikikojima0531@icloud.com>
2019-11-02 22:50:46 +0900

    A: comment

--- app/src/main/java/app/kojima/jiko/sutepro2/MainActivity.kt
+++ app/src/main/java/app/kojima/jiko/sutepro2/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    // This is lifecycle hook of onCreate...
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
